7660891b78a1d23cf74944e041ea4c606be53624
dylanmullarkey <DylanMullarkey10@gmail.com>
2023-01-23 14:59:56 -0500

    Initial commit

---
+++ src/main/java/PersonGenerator.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class PersonGenerator
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> people = new ArrayList<>();
+        boolean done = false;
+
+    /*    a. ID (a String)
+        b. FirstName
+        c. LastName
+        d. Title (a string like Mr., Mrs., Ms., Dr., etc.)
+        e. YearOfBirth (an int)
+
+     */
+        String id = "";
+        String firstName = "";
+        String lastName = "";
+        String title = "";
+        int YOB = 0;
+
+        do {
+            ID = SafeInput
+        }while (!done);
+
+
+    }
+}
